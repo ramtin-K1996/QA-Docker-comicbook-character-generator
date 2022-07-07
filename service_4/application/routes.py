169e4eac0adeffea5c46e9158eaa7239d6dabdf7
ramtin-K1996 <ramtinartemisk@gmail.com>
2022-07-07 17:06:25 +0000

    design updated

--- service_4/application/routes.py
+++ service_4/application/routes.py
@@ -9,7 +9,7 @@ def character():
     #different chances for superhero depending on 'power level' 
 
     if data_sent['power'] == "intelligence" and data_sent['publisher'] == "Marvel":
-        superhero= choice(["IRONMAN","mister fantastic","mister fantastic","mister fantastic"])
+        superhero= choice(["IRONMAN","mr fantastic","mr fantastic","mr fantastic"])
 
     elif data_sent['power'] == "strength" and data_sent['publisher'] == "Marvel":
         superhero= choice(["HULK","Spider-man","Spider-man","Spider-man","Spider-man"])
